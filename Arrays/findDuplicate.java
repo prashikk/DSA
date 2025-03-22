519bd25b38f645334b7aad3eb8fbcbff25da7114
Prashik Khotkar <91461422+prashikk@users.noreply.github.com>
2025-03-22 13:20:25 +0530

    findDuplicate.java

--- Arrays/findDuplicate.java
+++ Arrays/findDuplicate.java
@@ -4,7 +4,7 @@ public class findDuplicate {
     private static int duplicateNumber(int arr[]) {
         for(int i = 0 ; i < arr.length ; i++){
             for(int j = i + 1 ; j < arr.length ; j++){
-                if(arr[i] == arr[i]){
+                if(arr[i] == arr[j]){
                     return arr[i];
                 }
             }
